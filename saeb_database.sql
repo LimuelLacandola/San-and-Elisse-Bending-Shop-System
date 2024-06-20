54dba0a46fddbf2a3812c6234443bd61650c9376
Limuel Lacandola <lacandola.l.bsinfotech@gmail.com>
2024-06-20 15:28:18 +0800

    sql

---
+++ saeb_database.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 20, 2024 at 09:27 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `saeb_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `action_log`
+--
+-- Error reading structure for table saeb_database.action_log: #1932 - Table &#039;saeb_database.action_log&#039; doesn&#039;t exist in engine
+-- Error reading data for table saeb_database.action_log: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `saeb_database`.`action_log`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daily_inventory`
+--
+-- Error reading structure for table saeb_database.daily_inventory: #1932 - Table &#039;saeb_database.daily_inventory&#039; doesn&#039;t exist in engine
+-- Error reading data for table saeb_database.daily_inventory: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `saeb_database`.`daily_inventory`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+-- Error reading structure for table saeb_database.inventory: #1932 - Table &#039;saeb_database.inventory&#039; doesn&#039;t exist in engine
+-- Error reading data for table saeb_database.inventory: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `saeb_database`.`inventory`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log`
+--
+-- Error reading structure for table saeb_database.log: #1932 - Table &#039;saeb_database.log&#039; doesn&#039;t exist in engine
+-- Error reading data for table saeb_database.log: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `saeb_database`.`log`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+-- Error reading structure for table saeb_database.login: #1932 - Table &#039;saeb_database.login&#039; doesn&#039;t exist in engine
+-- Error reading data for table saeb_database.login: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `saeb_database`.`login`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification_history`
+--
+-- Error reading structure for table saeb_database.notification_history: #1932 - Table &#039;saeb_database.notification_history&#039; doesn&#039;t exist in engine
+-- Error reading data for table saeb_database.notification_history: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `saeb_database`.`notification_history`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset`
+--
+-- Error reading structure for table saeb_database.password_reset: #1932 - Table &#039;saeb_database.password_reset&#039; doesn&#039;t exist in engine
+-- Error reading data for table saeb_database.password_reset: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `saeb_database`.`password_reset`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restock_history`
+--
+-- Error reading structure for table saeb_database.restock_history: #1932 - Table &#039;saeb_database.restock_history&#039; doesn&#039;t exist in engine
+-- Error reading data for table saeb_database.restock_history: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `saeb_database`.`restock_history`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `return_history`
+--
+-- Error reading structure for table saeb_database.return_history: #1932 - Table &#039;saeb_database.return_history&#039; doesn&#039;t exist in engine
+-- Error reading data for table saeb_database.return_history: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `saeb_database`.`return_history`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+-- Error reading structure for table saeb_database.supplier: #1932 - Table &#039;saeb_database.supplier&#039; doesn&#039;t exist in engine
+-- Error reading data for table saeb_database.supplier: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `saeb_database`.`supplier`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction_history`
+--
+-- Error reading structure for table saeb_database.transaction_history: #1932 - Table &#039;saeb_database.transaction_history&#039; doesn&#039;t exist in engine
+-- Error reading data for table saeb_database.transaction_history: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `saeb_database`.`transaction_history`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction_product_details`
+--
+-- Error reading structure for table saeb_database.transaction_product_details: #1932 - Table &#039;saeb_database.transaction_product_details&#039; doesn&#039;t exist in engine
+-- Error reading data for table saeb_database.transaction_product_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `saeb_database`.`transaction_product_details`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
